21676ced21402102044f3b124fc69d074b1e4b49
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-04-23 16:24:41 +0900

    Start React with Next.js

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1 className="text-2xl font-semibold text-center">
+        Hello React. You are inevitable!
+      </h1>
+    </main>
+  )
+}
