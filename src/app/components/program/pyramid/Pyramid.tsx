c9cac646fc6e5cb294b74b2838d281936048e0b6
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-08-06 23:42:24 +0900

    refactor(day components): rename files
    renamed files so there would be fewer of them.

--- src/app/components/program/pyramid/Pyramid.tsx
+++ src/app/components/program/pyramid/Pyramid.tsx
@@ -1,3 +1,72 @@
-import PyramidInfo from '@/components/program/pyramid/PyramidInfo';
+'use client';
 
-export default PyramidInfo;
+import { useState } from "react";
+import styles from './Pyramid.module.css';
+import RepsCompleteButton from '@/components/program/pyramid/RepsCompleteButton';
+import {isSingular} from "@/utils";
+import MissModalPortal from "@/components/program/pyramid/MissModalPortal";
+import MaxoutNumberButton from "@/components/program/pyramid/MaxoutNumberButton";
+import DayComplete from "@/components/program/DayComplete";
+import PyramidDisplay from "@/components/program/pyramid/PyramidDisplay";
+
+const Pyramid = () => {
+  let initialRepsArray: number[] = [];
+
+  const [reps, setReps] = useState(1);
+  const [repsArray, setRepsArray] = useState(initialRepsArray);
+  const [missed, setMissed] = useState(false);
+  const [dayComplete, setDayComplete] = useState(false);
+
+  return (
+    <section className={styles.pyramidSectionContainer}>
+      <PyramidDisplay repsArray={repsArray} />
+      {dayComplete ? (
+        <DayComplete />
+      ) : (
+        <>
+
+
+      {missed ? (
+        <h3 className={styles.doRepsText}>MAX OUT!</h3>
+      ) : (
+        <h3 className={styles.doRepsText}>DO {reps} {isSingular(reps) ? 'REP' : 'REPS'}</h3>
+      )}
+      <div className={ styles.actionButtonContainer }>
+        {repsArray.length > 0 && !missed  && (
+        <MissModalPortal
+          onMissed={setMissed}
+          repsState={reps}
+          repsArrayState={repsArray}
+          setStateForReps={setReps}
+          setStateForRepsArray={setRepsArray}
+        />
+        )}
+        {missed ? (
+          <div className={styles.maxoutRepNumberContainer}>
+            {repsArray.map((reps) => {
+              return (
+                <MaxoutNumberButton
+                  repCount={reps}
+                  repsArrayState={repsArray}
+                  setStateForRepsArray={setRepsArray}
+                  setStateForDayComplete={setDayComplete}
+                />
+              )
+            })}
+          </div>
+        ) : (
+        <RepsCompleteButton
+          repsState={reps}
+          repsArrayState={repsArray}
+          setStateForReps={setReps}
+          setStateForRepsArray={setRepsArray}
+        />
+        )}
+      </div>
+        </>
+      )}
+    </section>
+  )
+};
+
+export default Pyramid;
--- src/app/program/day-five/page.tsx
+++ src/app/program/day-five/page.tsx
@@ -2,6 +2,10 @@ import { Metadata } from 'next';
 import styles from './page.module.css';
 import { DAYS } from '@/const';
 import { PageLink } from '@/components/PageLink';
+import FiveMaxEffortSets from '@/components/program/fiveMaxEffortSets/FiveMaxEffortSets';
+import Pyramid from '@/components/program/pyramid/Pyramid';
+import ThreeTrainingSetsThreeGrips from '@/components/program/threeTrainingSetsThreeGrips/ThreeTrainingSetsThreeGrips';
+import MaxTrainingSets from '@/components/program/maxTrainingSets/MaxTrainingSets';
 
 export const metadata: Metadata = {
   title: "Day Five | Armstrong Pull-up Program",
