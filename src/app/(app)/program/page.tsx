4acbd4a36023bcb58c4288456ef5216fa4012c8d
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-09-08 02:39:13 +0900

    feat(service worker): begin implementation

--- src/app/(app)/program/page.tsx
+++ src/app/(app)/program/page.tsx
@@ -3,10 +3,23 @@
 import {nunito} from '@/fonts';
 import styles from './page.module.css';
 import dynamic from 'next/dynamic';
+import {useEffect} from 'react';
 
 const Program = dynamic(() => import('@/components/program/Program'), { ssr: false });
 
 const ProgramPage = () => {
+
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      registerServiceWorker();
+    }
+  }, []);
+
+  async function registerServiceWorker() {
+    const registration = await navigator.serviceWorker.register('/service-worker.js',{
+      scope: '/program',
+    });
+  }
 
   return (
     <main className={styles.main}>
