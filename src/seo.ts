183bd4f1420462b504922c273cfc0f0d746dbc26
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-09-12 20:09:24 +0900

    feat(seo): add opengraph metadata
    Might need to be moved to be implemented

---
+++ src/seo.ts
@@ -0,0 +1,45 @@
+import { Metadata } from "next";
+
+export const defaultMetadata: Metadata = {
+  metadataBase: new URL('https://armstrong-pull-up-program.vercel.app/'),
+  alternates: {
+    canonical: '/',
+  },
+  title: {
+    default: 'Armstrong Pull-up Program',
+    template: '%s | Armstrong Pull-up Program'
+  },
+  description: 'A progressive web app that is based on the Armstrong Pull-up Program. It increases your maximum number of pullups with a 5-day training routine.',
+  applicationName: 'Armstrong Pull-up Program',
+  keywords: [
+    'pull-ups',
+    'pullups',
+    'health',
+    'fitness',
+    'health & fitness',
+    'progressive web app',
+    'pull-up program'
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://armstrong-pull-up-program.vercel.app/",
+    siteName: "Armstrong Pull-up Program App",
+    title: "Armstrong Pull-up Program App",
+    description: 'A progressive web app that is based on the Armstrong Pull-up Program. It increases your maximum number of pullups with a 5-day training routine.',
+    images: [
+      {
+        url: '/images/opengraph-wide-1080x473.png',
+        width: 1080,
+        height: 473,
+        alt: "Armstrong Pull-up Program",
+      },
+    ],
+  },
+  twitter: {
+    creator: "@handle",
+    title: "Armstrong Pull-up Program App",
+    site: "@site",
+    card: "summary_large_image",
+    images: ['/images/opengraph-wide-1080x473.png']
+  },
+};
