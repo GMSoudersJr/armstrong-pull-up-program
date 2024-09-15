8cb8542af9dadfc0e3ab916ac061a3405e21004f
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-09-15 20:55:26 +0900

    feat: add serwist package
    Getting the service worker to cache files was becoming a pain in the
    neck. Using this package to make it simpler.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'photos.google.com',
-        port: ''
-      },
-    ],
-  },
-  transpilePackages: ['lucide-react']
-};
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD
+} = require("next/constants");
 
-module.exports = nextConfig
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'photos.google.com',
+          port: ''
+        },
+      ],
+    },
+    transpilePackages: ['lucide-react']
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "src/app/sw.ts",
+      swDest: "public/sw.js",
+      cacheOnNavigation: true,
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
+}
