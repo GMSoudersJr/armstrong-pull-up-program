d06807cb2c9bce0833c7de9e9cb644154feaad61
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2024-09-16 18:50:58 +0900

    fix(PWA): update manifest and service worker
    add scope to manifest and comment out service worker calls in program
    page

--- src/app/(app)/program/page.tsx
+++ src/app/(app)/program/page.tsx
@@ -10,6 +10,7 @@ const OverallProgess = dynamic(() => import('@/components/program/OverallProgres
 
 const ProgramPage = () => {
 
+  /*
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       registerServiceWorker();
@@ -47,6 +48,7 @@ const ProgramPage = () => {
     window.location.reload();
   }
 
+   */
 
   return (
     <main className={styles.main}>
